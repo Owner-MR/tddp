fa69763695258b4ab3518da8679a1989678fa26b
Wytmr1518 <2224769764@qq.com>
2023-06-08 23:38:52 +0800

    使用Lua脚本实现分布式锁的原子性，避免删除锁时的并发安全问题

--- src/main/java/com/hmdp/utils/ILockImpl.java
+++ src/main/java/com/hmdp/utils/ILockImpl.java
@@ -1,8 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class ILockImpl implements ILock {
@@ -17,6 +21,12 @@ public class ILockImpl implements ILock {
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unLock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     @Override
     public boolean tryLock(long timeoutSec) {
         String key = KEY_PREFIX + name;
@@ -27,13 +37,20 @@ public class ILockImpl implements ILock {
         //防止success为null，拆箱时空指针异常
         return Boolean.TRUE.equals(success);
     }
-
     @Override
+    public void unLock(){
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+                );
+    }
+    /*@Override
     public void unLock() {
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         String lockThreadId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
         if (lockThreadId != null && lockThreadId.equals(threadId)){
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
